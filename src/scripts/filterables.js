8089e46f664596a2a8f37cda2bbff0d406f2c125
Rinku Kumari <rinkuk1993@gmail.com>
2019-05-28 08:38:15 +0530

    Update filterables.js

--- src/scripts/filterables.js
+++ src/scripts/filterables.js
@@ -15,4 +15,26 @@ const XRideFilterables = [
   'Car_Cancellation'
 ];
 
+const checkEmptyData = [
+  'id',
+  'user_id',
+  'vehicle_model_id',
+  'package_id',
+  'travel_type_id',
+  'from_area_id',
+  'to_area_id',
+  'from_city_id',
+  'to_city_id',
+  'from_date',
+  'to_date',
+  'online_booking',
+  'mobile_site_booking',
+  'booking_created',
+  'from_lat',
+  'from_long',
+  'to_lat',
+  'to_long',
+  'Car_Cancellation',
+];
+
 export default XRideFilterables;
